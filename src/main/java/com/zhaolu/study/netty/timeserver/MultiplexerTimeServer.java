3656950f7c1ffe4c8e3033510c8f1cc7cb4d80ed
zhaolu <lu.zhao255@gmail.com>
2017-12-01 18:21:19 +0800

    additional File socket selector demo

---
+++ src/main/java/com/zhaolu/study/netty/timeserver/MultiplexerTimeServer.java
@@ -0,0 +1,64 @@
+package com.zhaolu.study.netty.timeserver;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Administrator on 2017/12/1.
+ */
+public class MultiplexerTimeServer implements Runnable {
+    private Selector selector;
+    private ServerSocketChannel serverSocketChannel;
+    private volatile boolean stop;
+
+    public MultiplexerTimeServer(int port) {
+        try {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port),1024);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("The time server is start in port ： " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        if (Objects.nonNull(key)) {
+                            key.cancel();
+
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/zhaolu/study/netty/timeserver/TimeServer.java
@@ -0,0 +1,18 @@
+package com.zhaolu.study.netty.timeserver;
+
+/**
+ * Created by Administrator on 2017/12/1.
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                //采用默认值
+            }
+        }
+
+    }
+}
